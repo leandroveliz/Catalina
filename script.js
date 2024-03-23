4a5d410b2036b45abd4d8b0e365b501a02da35b1
Leandro Veliz <leanvelizcrc@gmail.com>
2024-03-23 20:35:53 -0300

    se cambian detalles en index

--- script.js
+++ script.js
@@ -79,3 +79,24 @@ balls.forEach((el, i, ra) => {
     }
   );
 });
+
+const cargarSonido = function (fuente) {
+    const sonido = document.createElement("audio");
+    sonido.src = fuente;
+    sonido.setAttribute("preload", "auto");
+    sonido.setAttribute("controls", "none");
+    sonido.style.display = "none"; // <-- oculto
+    document.body.appendChild(sonido);
+    return sonido;
+  };
+  const $botonReproducir = document.querySelector("#btnReproducir");
+    //$botonPausar = document.querySelector("#btnPausar");
+    //$botonReiniciar = document.querySelector("#btnReiniciar");*/
+  // El sonido que podemos reproducir o pausar
+  const sonido = cargarSonido("./imagen/Carlos rivera te esperaba.mp3");
+  /*$botonReproducir.onclick = () => {
+    sonido.play();
+  };
+  $botonPausar.onclick = () => {
+    sonido.pause();
+  };*/
